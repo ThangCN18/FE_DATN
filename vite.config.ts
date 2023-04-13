f61a037cd66cd85dc4670846513a90a89744df29
Nguyen Chi Thang <thang49082@donga.edu.vn>
2023-04-13 11:45:42 +0700

    fix js file vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve, parse } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    
     rollupOptions: {
-      output: {
-        assetFileNames: (asset) => {
-          if (parse(asset.name).name === 'externalImage') {
-            return "images/src/[name][extname]";
-          }
-          return "assets/[name].[hash][extname]";
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
         }
-      },
-    },
+    }
 }
 })
