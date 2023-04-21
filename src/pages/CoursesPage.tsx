56783e18e368e339a2013917df4abb4044407c13
Nguyen Chi Thang <thang49082@donga.edu.vn>
2023-04-21 11:28:32 +0700

    loading page courses

--- src/pages/CoursesPage.tsx
+++ src/pages/CoursesPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Badge, Breadcrumb, Button, Card, Col, Empty, Layout, Menu, Rate, Row, Skeleton, Space, Tabs, theme } from 'antd';
+import { Avatar, Badge, Breadcrumb, Button, Card, Col, Divider, Empty, Layout, Menu, Rate, Row, Skeleton, Space, Spin, Tabs, theme } from 'antd';
 import HeaderComponent from "../components/HeaderComponent";
 import SlideBanner from "../components/SlideBanner";
 import SliderRoadmap from "../components/SliderRoadmap";
@@ -21,6 +21,7 @@ import type { TabsProps } from 'antd';
 import SkeletonImage from "antd/es/skeleton/Image";
 import SkeletonInput from "antd/es/skeleton/Input";
 import SkeletonButton from "antd/es/skeleton/Button";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 function CoursesPage() {
@@ -30,12 +31,13 @@ function CoursesPage() {
     const loading = useSelector((state: RootState) => state.root.load)
     const [loaddingas, setloaddingas] = useState(false)
     const [datacourses, setdatacourses] = useState<any[]>([])
-    const [dataroadmap, setdataroadmap] = useState([])
+    const [pagenum, setpagenum] = useState(1)
     const auth = useSelector((state: RootState) => state.root.auth)
     const dispatch = useDispatch()
     const navigate = useNavigate();
-    const [datasub, setdatasub] = useState([])
+    const [loadspin, setloadspin] = useState(false)
     const [dataadacss, setdataadacss] = useState([])
+    const [datada, setdatada] = useState([])
 
 
 
@@ -51,41 +53,77 @@ function CoursesPage() {
 
     const hangdlegetdatacourses = async () => {
 
+        if (pagenum == 1) {
+            setloaddingas(true)
 
-        setloaddingas(true)
+
+        } else {
+            setloadspin(true)
+        }
+
         if (auth.isAuthenticated) {
             await getCourseSubscribe()
         }
         await hangdlegetdataroadmap()
 
-        await api.get('/courses/?page=1',
+        await api.get('/courses/?perPage=8&page=' + pagenum,
 
         ).then((response: any) => {
             setdatacourses(response.data.items)
+            setdatada(response.data.items)
+            if (pagenum == 1) {
+                var caca = [{
+                    key: 1,
+                    label: <p>All</p>,
+                    children: <Row gutter={[24, 24]} className="max-sm:px-6 mt-5">
+                        {!response.data.items ? null : <>
+                            {response.data.items.map(course => {
+                                console.log(aaaa)
+                                if (aaaa.includes(course.id)) {
 
-            const caca = [{
-                key: 1,
-                label: <p>All</p>,
-                children: <Row gutter={[24, 24]} className="max-sm:px-6 mt-5">
-                    {!response.data.items ? null : <>
-                        {response.data.items.map(course => {
-                            console.log(aaaa)
-                            if (aaaa.includes(course.id)) {
+                                    return <CardCourseItemComponent course={course} key={course.id} issub={true} />
+                                }
+                                return <CardCourseItemComponent course={course} key={course.id} issub={false} />
 
-                                return <CardCourseItemComponent course={course} key={course.id} issub={true} />
-                            }
-                            return <CardCourseItemComponent course={course} key={course.id} issub={false} />
+                            })}
+                        </>}
+                    </Row>
+                }]
 
-                        })}
-                    </>}
-                </Row>
-            }]
+            } else {
 
-            setdataadacss([...caca, ...ccccaa])
+                const dadaada = [...datacourses, ...response.data.items]
+                var caca = [{
+                    key: 1,
+                    label: <p>All</p>,
+                    children: <Row gutter={[24, 24]} className="max-sm:px-6 mt-5">
+                        {!dadaada ? null : <>
+                            {dadaada.map(course => {
+                                console.log(aaaa)
+                                if (aaaa.includes(course.id)) {
+
+                                    return <CardCourseItemComponent course={course} key={course.id} issub={true} />
+                                }
+                                return <CardCourseItemComponent course={course} key={course.id} issub={false} />
+
+                            })}
+                        </>}
+                    </Row>
+                }]
+            }
+            setpagenum(pagenum + 1)
+            if (caca.length > 0) {
+                setdataadacss([...caca, ...ccccaa])
+
+            }
             setloaddingas(false)
+            setloadspin(false)
+
         }).catch((error: any) => {
             console.log(error)
             setloaddingas(false)
+            setloadspin(false)
+
         })
 
     }
@@ -96,7 +134,7 @@ function CoursesPage() {
 
     const hangdlegetdataroadmap = async () => {
 
-        setloaddingas(true)
+
 
         await api.get('/roadmaps',
 
@@ -173,101 +211,111 @@ function CoursesPage() {
     return (
         <Layout className="layout bg-white">
             <HeaderComponent item="courses" />
-            <Content className="pt-[70px]">
-                <div className="site-layout-content w-[100%] pb-10 max-w-[1400px] mx-auto" style={{ background: colorBgContainer }}>
-                    <div className="max-w-[1400px] max-md:w-[90%] mx-auto text-center rounded-3xl px-[5%] py-[2%] bg-[rgba(71,213,226,0)] tapscoursescomponemt">
-                        {
-                            loaddingas ? <>
-                                <div className="flex justify-center space-x-2 ">
-                                    <SkeletonButton active className="my-4" />
-                                    <SkeletonButton active className="my-4" />
-                                    <SkeletonButton active className="my-4" />
-                                    <SkeletonButton active className="my-4" />
+            <InfiniteScroll
+                dataLength={datacourses.length}
+                next={hangdlegetdatacourses}
+                hasMore={datada.length >= 8}
+                loader={loadspin ? <div className="mx-auto w-[100%] flex justify-center mb-10"><Spin></Spin></div> : null}
+                endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+                scrollableTarget="scrollableDiv"
+            >
+                <Content className="pt-[70px]">
+                    <div className="site-layout-content w-[100%] pb-10 max-w-[1400px] mx-auto" style={{ background: colorBgContainer }}>
+                        <div className="max-w-[1400px] max-md:w-[90%] mx-auto text-center rounded-3xl px-[5%] py-[2%] bg-[rgba(71,213,226,0)] tapscoursescomponemt">
+                            {
+                                loaddingas ? <>
+                                    <div className="flex justify-center space-x-2 ">
+                                        <SkeletonButton active className="my-4" />
+                                        <SkeletonButton active className="my-4" />
+                                        <SkeletonButton active className="my-4" />
+                                        <SkeletonButton active className="my-4" />
 
-                                </div>
+                                    </div>
 
-                                <Row gutter={[24, 24]}>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                    <Row gutter={[24, 24]}>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                        </Col>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                        </Col>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                        </Col>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                        </Col>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                        </Col>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                        </Col>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={6} >
-                                        <div className="border-collapse border-[1px] rounded-md shadow-md">
-                                            <SkeletonButton active className="!w-[100%] !h-[175px]" />
-                                            <div className="px-3">
-                                                <Skeleton active className="my-5" />
-                                                <SkeletonButton active className="!w-[100%] mb-3" />
+                                        </Col>
+                                        <Col xs={24} sm={24} md={12} lg={12} xl={6} >
+                                            <div className="border-collapse border-[1px] rounded-md shadow-md">
+                                                <SkeletonButton active className="!w-[100%] !h-[175px]" />
+                                                <div className="px-3">
+                                                    <Skeleton active className="my-5" />
+                                                    <SkeletonButton active className="!w-[100%] mb-3" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </Col>
-                                </Row>
+                                        </Col>
+                                    </Row>
 
-                            </> :
-                                <Tabs defaultActiveKey="1" animated className="min-h-[475px]" items={items} onChange={onChange} />
-                        }
+                                </> :
+                                    <Tabs defaultActiveKey="1" animated className="min-h-[475px]" items={items} onChange={onChange} />
+                            }
 
+                        </div>
                     </div>
-                </div>
-            </Content>
+                </Content>
+            </InfiniteScroll>
+
             <FooterComponent />
             <Footer style={{ textAlign: 'center' }}>Copyright ©2023 Wizcove IT</Footer>
             {
